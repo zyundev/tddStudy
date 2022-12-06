88ea63926e485b57d62e8ca6a6f02262ca44497d
zyundev <jyi1102@hotmail.com>
2022-12-06 20:52:42 +0900

    TDD Basic 2-2 ~ 2-3

--- src/test/java/chep02/PasswordStrengthMeter.java
+++ src/test/java/chep02/PasswordStrengthMeter.java
@@ -2,6 +2,31 @@ package chep02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
+        //두번째 테스트
+        if (s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+        //세번째 테스트
+        //boolean containsNumber = false;
+        //for (char c: s.toCharArray()) {
+        //    if (c >= '0' && c <= '9') {
+        //        containsNumber = true;
+        //        break;
+        //    }
+        //} -> 메소드로 리팩토링
+        boolean containsNumber = meetsContainingNumberCriteria(s);
+        if (!containsNumber) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
+    }
+
+    public boolean meetsContainingNumberCriteria(String s) {
+        for (char c: s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/chep02/PasswordStrengthMeterTest.java
+++ src/test/java/chep02/PasswordStrengthMeterTest.java
@@ -5,10 +5,42 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    /**
+     * 첫번째 테스트
+     */
     @Test
     void meetsAllCriteria_Then_Strong() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        //PasswordStrengthMeter meter = new PasswordStrengthMeter(); 중복제거
         PasswordStrength result = meter.meter("ab12!@AB");
         assertEquals(PasswordStrength.STRONG, result);
+    }
+
+    /**
+     * 두번째 테스트
+     */
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        //PasswordStrengthMeter meter = new PasswordStrengthMeter(); 중복제거
+        PasswordStrength result = meter.meter("ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+
+    /**
+     * 세번째 테스트
+     */
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        //중복제거
+        //PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        //PasswordStrength result = meter.meter("ab!@ABsdre");
+        //assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab!@ABsdre", PasswordStrength.NORMAL);
+    }
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
     }
 }
